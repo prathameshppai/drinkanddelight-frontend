f897b72b8358f18b6db25c6b89c0386bd2afc058
Prathamesh Pai <prathameshppai@gmail.com>
2019-10-16 15:35:36 +0530

    place raw material order working

--- src/app/rawmaterials/place-rawmaterial-orders/place-rawmaterial-orders.service.ts
+++ src/app/rawmaterials/place-rawmaterial-orders/place-rawmaterial-orders.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class PlaceRawmaterialOrdersService {
 
   getPlaceRawmaterialOrderMessage(RMNameVar: string, SUPIDVar: string ,quantityVar: number, QuantityUnitVar: string, expectedDateofDeliveryVar: Date , pricePerUnitVar: number, warehouseIdVar: string): Observable<String> {
-    return this.http.post<string>("http://localhost:9090/drinkanddelight/PlaceRMOrderServlet",{"rmname": RMNameVar, "supplierId": SUPIDVar,"quantityValue" : quantityVar,"quantityUnit": QuantityUnitVar, "dateOfDelivery" : expectedDateofDeliveryVar, "pricePerUnit" : pricePerUnitVar, "warehouseId": warehouseIdVar});
+    return this.http.post<string>("http://localhost:9090/drinkanddelight/PlaceRMOrderServlet",{"name": RMNameVar, "supplierId": SUPIDVar,"quantityValue" : quantityVar,"quantityUnit": QuantityUnitVar, "dateOfDelivery" : expectedDateofDeliveryVar, "pricePerUnit" : pricePerUnitVar, "warehouseId": warehouseIdVar});
   }
 
   constructor(private http: HttpClient) { }
