0634e3d277e8944610019fed4ca399c4eb4c7310
Prathamesh Pai <prathameshppai@gmail.com>
2019-10-19 14:56:04 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RawmaterialsModule } from './rawmaterials/rawmaterials.module';
 import { ProductsModule } from './products/products.module';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-page', component: HomepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), 
