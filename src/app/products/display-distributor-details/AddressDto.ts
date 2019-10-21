7ff98fce782cad17c83415fffd0b1f84a1958826
akhi-molu <55138267+akhi-molu@users.noreply.github.com>
2019-10-21 18:00:20 +0530

    changes

--- src/app/products/display-distributor-details/AddressDto.ts
+++ src/app/products/display-distributor-details/AddressDto.ts
@@ -1,10 +1,10 @@
 export interface Address {
-	  plotNo : number ;
-	  buildingName :string;	
-	  streetName :string;	
-	  landmark  :string;
-	  city :string;
-	  pincode :string;
-	  state :string;
-      defaultAddress : boolean;	
+	plotNo : number ;
+	buildingName :string;	
+	streetName :string;	
+	landmark  :string;
+	city :string;
+	pincode :string;
+	state :string;
+	defaultAddress : boolean;	
 }
