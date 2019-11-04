87c9da778b36c6eb073166488d6aedf01c8ac4ba
adkeaesph420 <55137956+adkeaesph420@users.noreply.github.com>
2019-11-04 13:22:18 +0530

    authentication updated on front end

--- src/app/authentication/username-existence/username-existence.service.ts
+++ src/app/authentication/username-existence/username-existence.service.ts
@@ -9,6 +9,8 @@ export class UsernameExistenceService {
   constructor(private http: HttpClient) { }
 
   getUsernameExistenceMessage(usernameVar: string): Observable<string> {
-    return this.http.post<string>("http://localhost:9090/drinkanddelight/username-existence",{"username": usernameVar});
+    return this.http.post<string>("http://localhost:9090/drinkanddelight/username-existence",{
+      "username": usernameVar
+    });
   }
 }
