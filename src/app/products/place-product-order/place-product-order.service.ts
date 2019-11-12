9661bf42203835158835b747b6ca9d46d5235c0f
Prathamesh Pai <prathameshppai@gmail.com>
2019-11-12 16:48:55 +0530

    Update place-product-order.service.ts

--- src/app/products/place-product-order/place-product-order.service.ts
+++ src/app/products/place-product-order/place-product-order.service.ts
@@ -8,11 +8,11 @@ import { Observable } from 'rxjs';
 export class PlaceProductOrderService {
   
   getPlaceProductOrderMessage(ProductNameVar: string, DISTIDVar: string ,quantityVar: number, QuantityUnitVar: string, expectedDateofDeliveryVar: Date , pricePerUnitVar: number, warehouseIdVar: string): Observable<string>{
-    return this.http.post<string>("http://localhost:9090/drinkanddelight/placeProductOrder",{"name": ProductNameVar, "distributorId": DISTIDVar,"quantityValue" : quantityVar,"quantityUnit": QuantityUnitVar, "dateOfDelivery" : expectedDateofDeliveryVar, "pricePerUnit" : pricePerUnitVar, "warehouseId": warehouseIdVar});
+    return this.http.post<string>("http://localhost:9090/placeProductOrder",{"name": ProductNameVar, "distributorId": DISTIDVar,"quantityValue" : quantityVar,"quantityUnit": QuantityUnitVar, "dateOfDelivery" : expectedDateofDeliveryVar, "pricePerUnit" : pricePerUnitVar, "warehouseId": warehouseIdVar});
   }
 
   getProductDetailsList(): Observable<string>{
-    return this.http.get<string>("http://localhost:9090/drinkanddelight/GetProductDetails");
+    return this.http.get<string>("http://localhost:9090/GetProductDetails");
   }
   
   constructor(private http: HttpClient) { }
